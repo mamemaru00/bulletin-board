79e708c5a383da4e6f59da3078364c53d8dbe9f6
mamemaru00 <k.shouhei0711@gmail.com>
2023-01-08 19:53:50 +0900

    マイグレイト

--- database/migrations/2023_01_08_193606_create_replies_table.php
+++ database/migrations/2023_01_08_193606_create_replies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateThreadsTable extends Migration
+class CreateRepliesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateThreadsTable extends Migration
      */
     public function up()
     {
-        Schema::create('threads', function (Blueprint $table) {
+        Schema::create('replies', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('thread_id');
             $table->string('user_name', 20);
-            $table->string('message_title', 50);
             $table->string('message', 200);
             $table->timestamps();
         });
@@ -29,6 +29,6 @@ class CreateThreadsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('threads');
+        Schema::dropIfExists('replies');
     }
 }
